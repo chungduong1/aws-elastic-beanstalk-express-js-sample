1f44574920c68fa4789d71321a5f6a48c6004a54
Chung <21593837@student.curtin.edu.au>
2024-10-30 16:31:15 +0800

    change app.js

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
+// const express = require('express');
+// const app = express();
+// const port = 8080;
+
+// app.get('/', (req, res) => res.send('Hello World! My name Chung'));
+
+// app.listen(port);
+// console.log(`App running on http://localhost:${port}`);
 const express = require('express');
 const app = express();
 const port = 8080;
